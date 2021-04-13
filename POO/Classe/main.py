dde92c87a6eca35dc85bb5cf74cdb5ebb9ee14b3
Karla Pereira <karlapereira@gec.inatel.br>
2021-04-13 16:31:30 -0300

    Add files via upload

--- POO/Classe/main.py
+++ POO/Classe/main.py
@@ -1,1 +1,16 @@
+from pessoa import Pessoa
 
+# p1 = Pessoa()
+# p1.nome = 'Luiz'
+
+p1 = Pessoa('Karla', 25)
+p2 = Pessoa('João', 32)
+
+p1.comer('maçã')
+p1.falar('POO')
+p2.falar('Filmes')
+p1.parar_comer()
+p1.falar('POO')
+p1.comer('bolo')
+print(Pessoa.ano_atual)
+print(p1.get_ano_nascimento())
